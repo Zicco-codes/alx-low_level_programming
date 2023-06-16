4d91486e3b9dc553e8e14dd923d0de8be7f5c646
Zicco-codes <xeccokavie@gmail.com>
2023-06-15 18:15:16 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Programing using prntf
+ * return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
