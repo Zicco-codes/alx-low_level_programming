930e1d515605ac554c2e7fca1bb17f7e05e223e1
Zicco-codes <xeccokavie@gmail.com>
2023-06-18 16:14:17 -0700

    0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - main entry
+ * description - running a program that counts negative nmbers in a RAND_MAX
+ * Return - Always 0
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+		printf("%d is positive \n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero \n", n);
+	}
+	else
+	{
+		printf("%d is negative\n", n);
+	}
+	return (0);
+}
