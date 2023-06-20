27d29e428e1e2aeb7a206a55e608c45b6e450cc1
Zicco-codes <xeccokavie@gmail.com>
2023-06-19 17:58:58 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet in lowercase
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int n = 97;
+
+	while (n <= 122)
+	{
+		if (n == 101 || n == 113)
+		n++;
+		continue;
+	}
+	putchar(n);
+	n++;
+	return (0);
+}
