96f1b3365268e3064c9d5eed696b5ada08b81744
Zicco-codes <xeccokavie@gmail.com>
2023-06-22 19:20:54 -0700

    update

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -11,11 +11,11 @@
 */
 void print_remaining_days(int month, int day, int year)
 {
-	if ((year % 100 == 0 && year % 400 == 0) || !(year % 4 ==  0))
+	if (year % 4 == 0, (year % 400 == 0 && year % 100 == 0))
 	{
 		if (month > 2 && day >= 60)
 		{
-			d++
+			day++;
 		}
 		printf("Day of year: %d\n", day);
 		printf("Remaing days: %d\n", 366 - day);
